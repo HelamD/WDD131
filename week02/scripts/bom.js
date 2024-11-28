67600a31adeb6545449eed6899c7d5a5100c8c13
HelamD <helampathway@gmail.com>
2024-11-27 22:22:56 -0300

    BOM

---
+++ week02/scripts/bom.js
@@ -0,0 +1,27 @@
+const input = document.querySelector('#favchap');
+const addButton = document.querySelector('button');
+const list = document.querySelector('#list');
+
+
+addButton.addEventListener('click', function(){
+    const inputResponse = input.value;
+    input.value= '';
+
+    const listItem = document.createElement('li');
+    const deleteButton = document.createElement('button');
+
+    listItem.textContent = inputResponse;
+
+    deleteButton.textContent = '❌';
+
+    listItem.appendChild(deleteButton)
+    list.appendChild(listItem);
+
+    deleteButton.addEventListener('click', function(){
+        list.removeChild(listItem)
+    });
+
+    input.focus();
+})
+
+
